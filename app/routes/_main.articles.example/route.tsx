2156005ea903b616a4d65f480dbdec95f890acff
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2023-12-24 20:51:58 +0900

    support image insertion

--- app/routes/_main.articles.example/route.tsx
+++ app/routes/_main.articles.example/route.tsx
@@ -1,27 +1,33 @@
 import React from 'react';
-import Example from '~/articles/example.mdx';
+import * as Example from 'page.mdx';
 import Switch from '~/components/Switch.tsx';
 import Header1 from '~/components/articles/Header1.tsx';
 import Header2 from '~/components/articles/Header2.tsx';
 import Header3 from '~/components/articles/Header3.tsx';
 import Paragraph from '~/components/articles/Paragraph.tsx';
+import Quote from '~/components/articles/Quote.tsx';
 
 export default function Article(props: {}) {
   const [summarized, setSummarized] = React.useState(false);
+  const Component = Example.default;
   return (
     <>
-      <Switch
-        actionName="Summarize"
-        state={summarized}
-        onSwitch={(state: boolean) => setSummarized(state)}
-      />
-      <Example
+      <div className="flex justify-between items-center my-24">
+        <h1 className="text-geb-blue font-bold text-3xl">{Example.attributes.title}</h1>
+        <Switch
+          actionName="Summarize"
+          state={summarized}
+          onSwitch={(state: boolean) => setSummarized(state)}
+        />
+      </div>
+      <Component
         components={{
           h1: (props) => <Header1 {...props} />,
           h2: (props) => <Header2 {...props} />,
           h3: (props) => <Header3 {...props} />,
           p: (props) => <Paragraph {...{ ...props, summarized }} />,
           pre: (props) => <pre {...props} className="p-4 rounded-lg overflow-auto" />,
+          blockquote: (props) => <Quote {...props} />,
         }}
       />
     </>
