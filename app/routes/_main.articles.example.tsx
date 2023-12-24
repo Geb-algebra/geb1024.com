f548bbd4d37e966d46191fc6a4720afc50544420
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2023-12-24 12:37:55 +0900

    headers

--- app/routes/_main.articles.example.tsx
+++ app/routes/_main.articles.example.tsx
@@ -2,6 +2,8 @@ import type { LoaderFunctionArgs } from '@remix-run/node';
 import invariant from 'tiny-invariant';
 import Example from '~/articles/example.mdx';
 import Header1 from '~/components/articles/Header1.tsx';
+import Header2 from '~/components/articles/Header2.tsx';
+import Header3 from '~/components/articles/Header3.tsx';
 import Paragraph from '~/components/articles/Paragraph.tsx';
 
 export default function Article(props: {}) {
@@ -9,6 +11,8 @@ export default function Article(props: {}) {
     <Example
       components={{
         h1: (props) => <Header1 {...props} />,
+        h2: (props) => <Header2 {...props} />,
+        h3: (props) => <Header3 {...props} />,
         p: (props) => <Paragraph {...props} />,
         pre: (props) => <pre {...props} className="p-4 rounded-lg overflow-auto" />,
       }}
