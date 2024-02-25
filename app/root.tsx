45ea4f7bed4d97f50cbdf145466a71b07f66e62d
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2024-02-25 10:16:54 +0900

    use vite

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,8 @@
-import { cssBundleHref } from '@remix-run/css-bundle';
 import type { LinksFunction } from '@remix-run/node';
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
-import stylesheet from '~/styles/tailwind.css';
-import rdtStylesheet from 'remix-development-tools/index.css';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import stylesheet from '~/styles/tailwind.css?url';
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
   { rel: 'stylesheet', href: stylesheet },
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
   { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
@@ -13,7 +10,6 @@ export const links: LinksFunction = () => [
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap',
   },
-  ...(process.env.NODE_ENV === 'development' ? [{ rel: 'stylesheet', href: rdtStylesheet }] : []),
 ];
 
 function App() {
@@ -29,16 +25,9 @@ function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
       </body>
     </html>
   );
 }
 
-let AppExport = App;
-// This imports the dev tools only if you're in development
-if (process.env.NODE_ENV === 'development') {
-  const { withDevTools } = await import('remix-development-tools');
-  AppExport = withDevTools(AppExport);
-}
-export default AppExport;
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import { remixDevTools } from 'remix-development-tools/vite';
+import { installGlobals } from '@remix-run/node';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+installGlobals();
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    remixDevTools(),
+    remix({
+      ignoredRouteFiles: ['**/.*'],
+    }),
+    tsconfigPaths(),
+  ],
+  build: {
+    target: 'ES2022',
+  },
+});
