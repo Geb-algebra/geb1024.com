1febf403e9929eaab71214a033381ef58b6ac7f3
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2024-05-18 16:32:32 +0900

    update packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
-import { remixDevTools } from 'remix-development-tools/vite';
+import { remixDevTools } from 'remix-development-tools';
 import { installGlobals } from '@remix-run/node';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
