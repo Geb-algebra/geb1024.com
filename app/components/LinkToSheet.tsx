0d54c478ee597ff5a4bc635fde4ff23245272277
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2024-06-09 10:53:35 +0900

    fix radius

--- app/components/LinkToSheet.tsx
+++ app/components/LinkToSheet.tsx
@@ -4,7 +4,7 @@ export default function LinkToSheet(props: { to: string; children?: React.ReactN
   return (
     <Link
       to={props.to}
-      className="rounded-iconic-xl md:rounded-iconic-3xl hover:scale-105 hover:bg-paper-color hover:shadow-float transition-all duration-500"
+      className="rounded-iconic-3xl hover:scale-105 hover:bg-paper-color hover:shadow-float transition-all duration-500"
     >
       {props.children}
     </Link>
