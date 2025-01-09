3b6719c7231ee308741cf2f7ba36197cc36cdb89
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2025-01-09 21:34:01 +0900

    update to latest remix

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,14 @@ export default defineConfig({
       routes: async (defineRoutes) => {
         return flatRoutes("routes", defineRoutes);
       },
+      future: {
+        v3_singleFetch: true,
+        v3_fetcherPersist: true,
+        v3_lazyRouteDiscovery: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        unstable_optimizeDeps: true,
+      },
     }),
     tsconfigPaths(),
   ],
